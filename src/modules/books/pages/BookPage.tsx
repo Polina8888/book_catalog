cf1f923ae98085faef2d01bc19310b321347b528
mag08 <polina.arutyunyan.08@gmail.com>
2026-03-09 16:24:47 +0300

    Replace manual fetch with useQuery in BookPage

--- src/modules/books/pages/BookPage.tsx
+++ src/modules/books/pages/BookPage.tsx
@@ -1,18 +1,21 @@
 import { useParams } from "react-router-dom"
-import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { getBookById } from "../api/GoogleBooksApi";
 import type { Book } from "../types";
 import CartIcon from "../../cart/CartIcon";
 import { cartSlice } from '../../cart/cartSlice.ts'
 import { useAppDispatch } from '../../../store.ts'
+import { Spinner } from "react-bootstrap";
 import type { CartItem } from '../../cart/types.ts'
 import AlertComponent from "../components/Alert.tsx";
 
 
+
 function BookPage() {
-  const [book, setBook] = useState<Book>();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+  // const [book, setBook] = useState<Book>();
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState<string | null>(null);
   const { id } = useParams();
   const [showAlert, setShowAlert] = useState(false);
 
@@ -32,73 +35,79 @@ function BookPage() {
     dispatch(cartSlice.actions.addItem(cartItem))
   }
 
-  useEffect(() => {
-    if (!id) return;
+  // useEffect(() => {
+  //   if (!id) return;
 
-    setLoading(true);
+  //   setLoading(true);
 
-    getBookById(id)
-      .then(data => {
-        setBook(data);
-        console.log(data)
-      })
-      .catch(() => setError("Failed to load book"))
-      .finally(() => setLoading(false));
-  }, [id])
+  //   getBookById(id)
+  //     .then(data => {
+  //       setBook(data);
+  //       console.log(data)
+  //     })
+  //     .catch(() => setError("Failed to load book"))
+  //     .finally(() => setLoading(false));
+  // }, [id])
 
-  if (loading) return (
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['book', id],
+    queryFn: () => getBookById(id!),
+    enabled: !!id,
+  })
+
+  if (isLoading) return (
     <div className="d-flex justify-content-center align-items-center vh-100">
-      <p>Loading...</p>
+      <Spinner animation="border" className="m-5" />
     </div>
   )
 
-  if (error) return (
+  if (isError) return (
     <div className="d-flex justify-content-center align-items-center vh-100">
       <p>An error occurred</p>
     </div>
   )
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
+    <div className="d-flex justify-content-center align-items-center vh-100 scroll">
       <CartIcon />
       <AlertComponent showAlert={showAlert}></AlertComponent>
       <div className="container">
         <div className="row gy-4">
-          <div className="col col-md-6 d-flex flex-column justify-content-start gap-4">
+          <div className="col-sm col-md-6 d-flex flex-column justify-content-start gap-4">
             <div className="d-flex flex-column gap-2">
-              <h1 className="mb-0">{book?.volumeInfo.title}</h1>
-              {book?.volumeInfo.authors?.map((author) => (
+              <h1 className="mb-0">{data?.volumeInfo.title}</h1>
+              {data?.volumeInfo.authors?.map((author) => (
                 <h5 key={author} className="mb-1">
                   {author}
                 </h5>
               ))}
             </div>
           </div>
-          <div className="col col-md-6 d-flex align-items-center">
-            <h1>{book?.price} $</h1>
+          <div className="col-sm col-md-6 d-flex align-items-center">
+            <h1>{data?.price} $</h1>
           </div>
-          <div className="col col-md-6">
+          <div className="col-sm col-md-6">
             <div
               className="bg-light p-2"
               style={{ maxWidth: "25rem", height: "25rem" }}
             >
               <img
                 src={
-                  book?.volumeInfo.imageLinks?.thumbnail ??
+                  data?.volumeInfo.imageLinks?.thumbnail ??
                   "../../../assets/placeholder_book.png"
                 }
                 className="w-100 h-100 object-fit-contain"
-                alt={book?.volumeInfo.title}
+                alt={data?.volumeInfo.title}
               />
             </div>
           </div>
-          <div className="col col-md-6 d-flex flex-column gap-4">
-            <p className="p-4 border">{book?.volumeInfo.description}</p>
+          <div className="col-sm col-md-6 d-flex flex-column gap-4">
+            <div className="container p-4 border scroll" style={{ height: '50vh'}}>{data?.volumeInfo.description}</div>
             <button className="btn btn-primary w-100"
               onClick={(e) => {
                 e.stopPropagation();
-                handleAddToCart(book);
+                handleAddToCart(data);
                 setShowAlert(true);
-                setTimeout(() => setShowAlert(false), 500)
+                setTimeout(() => setShowAlert(false), 1000)
               }}>
               Add to cart
             </button>
